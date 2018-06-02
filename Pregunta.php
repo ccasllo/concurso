e4e9249fbce7f589432aabc6496aea54f347ab6e
ccasllo <castillocarlos19@gmail.com>
2018-06-02 10:10:23 -0500

    Update Pregunta.php

--- Pregunta.php
+++ Pregunta.php
@@ -74,12 +74,10 @@ $directorio2=utf8_encode (substr($d1,$posini,$posfin-$posini));
 
 $pre=utf8_encode ($directorio2);
 
-$file="Prueba1/U_1_Numero_y_algebra/Pre_Numero_y_algebra/".".$pregunta.".pdf";      
+$file="Prueba1/U_1_Numero_y_algebra/Pre_Numero_y_algebra/".$pregunta.".pdf";      
 echo $file;
 $pdf='http://mozilla.github.io/pdf.js/web/viewer.html?file=//'.$file;
 
-//$file=$home."\\".$directorio."\\"."$pre"."\\Pregunta".$unidad.".".$pregunta.".pdf";
-      
       
 // Prueba1\U_1_Numero_y_algebra\Pre_Numero_y_algebra
       
