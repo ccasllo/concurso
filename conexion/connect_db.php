06cfed1624c9297095cb92a3b31e1d4f4e2eb599
ccasllo <castillocarlos19@gmail.com>
2018-06-02 08:25:59 -0500

    Update connect_db.php

--- conexion/connect_db.php
+++ conexion/connect_db.php
@@ -1,6 +1,6 @@
 <?php
 						 //servidor ,usuario ,contraseña,base de datos
-	$mysqli = new MySQLi("localhost", "admin","admin", "CONCURSO");
+	$mysqli = new MySQLi("nuskkyrsgmn5rw8c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "sw21pev5toum5iqk","mp6tpf7841z3fhvs", "biy1q90u3vjgvpc9");
 	if ($mysqli -> connect_errno) {
 		die( "Fallo la conexión a MySQL: (" . $mysqli -> mysqli_connect_errno() 
 			. ") " . $mysqli -> mysqli_connect_error());
